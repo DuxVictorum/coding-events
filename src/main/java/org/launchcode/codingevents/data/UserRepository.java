cd52d2604ae46e2d9327146a5e5a2c50e3b63156
Mark Thorne <thorne.mark@gmail.com>
2021-12-27 10:09:43 -0600

    Updated UserRepository

--- src/main/java/org/launchcode/codingevents/data/UserRepository.java
+++ src/main/java/org/launchcode/codingevents/data/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
 
+//    Custom query method
     User findByUsername(String username);
 
 }
